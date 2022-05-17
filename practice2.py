e8eeede5aefa29aaf560992bb60689929c945ac4
Jaeyeon Son <chilling0612@gmail.com>
2022-05-17 19:37:05 +0900

    first commit

---
+++ practice2.py
@@ -0,0 +1,15 @@
+#숫자처리함수
+print(abs(-3))
+print(pow(4,5)) # 4^5 5만큼 제곱한 값이 나옴
+print(max(5,10))#최대값
+print(min(2,8))#최소값
+print(round(3.14))#반올림값 3
+print(round(3.99))#반올림값 4
+
+from math import * #math 라이브러리 데려오는거
+print(floor(4.99)) #내림 4
+print(ceil(3.01)) #올림 4
+print(sqrt(16))#제곱근 4
+
+from random import *
+print(random())
